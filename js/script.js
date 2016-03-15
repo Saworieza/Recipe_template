420610e3ff7aa81795f90bf490fdfcce34587786
Sawo <saworieza@gmail.com>
2016-03-15 14:56:38 +0300

    styling recipes 99% complete

--- js/script.js
+++
@@ -1,9 +0,0 @@
-('.SeeMore2').click(function(){
-		var $this = $(this);
-		$this.toggleClass('SeeMore2');
-		if($this.hasClass('SeeMore2')){
-			$this.text('See More');			
-		} else {
-			$this.text('See Less');
-		}
-	});
